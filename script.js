31b35eb04c447d2cbc0d3982a2186062ed90b84b
Kosenkov Viktor <kosenkov@intaro.email>
2022-04-12 01:55:48 +0300

    формы загрузки фото

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 const modal_auth = document.querySelector('#modal-wrapper-auth');
-const modal__close = document.querySelector('#modal__close');
+const modal_auth__close = document.querySelector('#modal__close');
+const btn_settings = document.querySelector('#btn_settings');
+const modal_load_photo = document.querySelector('#modal-load-photo');
+const btn_load_close = document.querySelector('#btn_close_photo');
+const avatarUserCustom = document.querySelector('#avatar-user-custom');
+const avatarUserDefault = document.querySelector('#avatar-user-default');
 
 
-modal__close.addEventListener('click', () => {
+btn_settings.addEventListener('click', () => {
+    modal_load_photo.classList.remove('invisible');
+});
+
+btn_load_close.addEventListener('click', () => {
+    modal_load_photo.classList.add('invisible');
+});
+
+modal_auth__close.addEventListener('click', () => {
     modal_auth.style.display = 'none';
 });
 
 window.addEventListener('click', (e) => {
-    if (e.target == modal_auth) {
+    if (e.target == modal_auth__close) {
         modal_auth.style.display = 'none';
     }
 });
 
-function toggleMenu(menu){
-    menu.classList.toggle('change');
-}
 
-function toCamelCase(str){
-    return str.split('-').map((word, index) => {
-        if(index == 0){
-            return word;
+const inputPhoto = document.querySelector('#inputPhoto');
+const fileReader = new FileReader();
+
+fileReader.addEventListener('load', () => {
+    let theImage = fileReader.result;
+    avatarUserCustom.classList.remove('invisible');
+    avatarUserDefault.style.display = 'none';
+    avatarUserCustom.src = theImage;
+});
+
+inputPhoto.addEventListener('change', (e) => {
+    const [file] = e.target.files;
+
+    if (file) {
+        if (file.size > 300 * 1024) {
+            alert('Слишком большой файл');
         } else {
-            console.log(word[0].toUpperCase() + word.slice(1));
-            console.log('slice', word.slice(1));
-            return word[0].toUpperCase() + word.slice(1);
+            fileReader.readAsDataURL(file);
         }
-    }).join('');
-}
-
-console.log(toCamelCase('the_stealth-warrior'));
+    }
+});
